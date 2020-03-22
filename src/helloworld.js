6d2ff6b17d0ae20f8143c1a66789de42aa337b99
MikolaLvivUA <mikola.shidlovsky@gmail.com>
2020-03-22 20:51:19 +0200

    my-first-serverless local - functioans

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,3 +1,9 @@
-module.exports.handler = (evt, ctx, done) => {
-  
-}
+module.exports.hello = (evt, ctx, done)/*(event, context, callback)*/ => {
+  const response = {
+      statusCode: 200,
+      data: evt,
+  };
+  done(null, response);
+};
+
+/*event.json it's our json file with our data*/
